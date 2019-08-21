77ae9d75946819c49919cd002117a356838d6dac
gorebill <gorebill@163.com>
2019-08-21 12:04:06 +0800

    init

---
+++ src/ios/UbibotPluginAliyun.m
@@ -0,0 +1,30 @@
+/********* UbibotPluginAliyun.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface UbibotPluginAliyun : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation UbibotPluginAliyun
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    NSLog(@"UbibotPluginAliyun print...");
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
